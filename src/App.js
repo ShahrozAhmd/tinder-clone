8f13b9cde712ac34c484104afe579a6fcc326e35
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-03-24 12:52:01 +0500

    set up the layout using material-UI flex apis and added auth button and card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, Grid, makeStyles } from "@material-ui/core";
+import react from "react";
+import Card from "./Components/cards";
+
+const useStyles = makeStyles({
+  container: {
+    border: "1px solid black",
+    height: "100vh",
+  },
+  i: {
+    border: "1px solid purple",
+    height: "20px",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-     
-    </div>
+    <Container className={classes.container} disableGutters maxWidth={1}>
+      <Box display="flex" conatiner flexDirection="row-reverse" p={2}>
+        <Box>
+          <Button variant="contained" color="secondary">
+            SignUp
+          </Button>
+        </Box>
+      </Box>
+      <Box display="flex" conatiner flexDirection="row" justifyContent = "center">
+        <Box>
+          <Card />
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
